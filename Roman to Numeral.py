e5f32a66b992c58b3a0c6a913cedbf85ab0376ef
AbilashC13 <46396583+AbilashC13@users.noreply.github.com>
2019-06-10 16:06:05 +0530

    Create Roman to Numeral.py

---
+++ Roman to Numeral.py
@@ -0,0 +1,9 @@
+s=input()
+r={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+v=0
+for i in range(len(s)):
+    if i>0 and r[s[i]]>r[s[i-1]]:
+        v+=r[s[i]]-2*r[s[i-1]]
+    else:
+        v+=r[s[i]]
+print(v)
